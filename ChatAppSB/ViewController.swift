7933f5dca35a192fe40d25daf26150290b964c83
Emre İçmen <icmenemre@gmail.com>
2024-12-20 07:17:47 +0300

    Initial Commit

---
+++ ChatAppSB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatAppSB
+//
+//  Created by Emre İÇMEN on 20.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
