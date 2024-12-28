45b516f1ee90afd3373acfec48eb6bd3fbe5f8cd
Emre İçmen <icmenemre@gmail.com>
2024-12-28 07:50:07 +0300

    Created attach and record button

--- ChatAppSB/Module/View/CustomInputView.swift
+++ ChatAppSB/Module/View/CustomInputView.swift
@@ -10,8 +10,11 @@ import UIKit
 
 
 protocol CustomInputViewDelegate: AnyObject {
+    
     func inputView(_ view: CustomInputView, wantUploadMessage message: String)
 }
+
+
 class CustomInputView: UIView {
     
     //MARK: - Properties
@@ -23,31 +26,57 @@ class CustomInputView: UIView {
         let imageView = CustomImageView(width: 40, height: 40, backgroundColor: MAIN_COLOR, cornerRadius: 20)
         imageView.isUserInteractionEnabled = true
         imageView.addGestureRecognizer(gestureRecognizer)
+        imageView.isHidden = true
         
         return imageView
     }()
-    
     private lazy var sendButton: UIButton = {
 
         let button = UIButton(type: .system)
         button.setBackgroundImage(UIImage(systemName: "paperplane.fill"), for: .normal)
         button.tintColor = .white
         button.addTarget(self, action: #selector(sendMessage), for: .touchUpInside)
-        button.setDimensions(height: 28, width: 28)
+        button.setDimensions(height: 30, width: 30)
+        button.isHidden = true
         
         return button
     }()
-    
+    private lazy var attachMediaButton: UIButton = {
+        
+        let button = UIButton(type: .system)
+        button.setBackgroundImage(UIImage(systemName: "paperclip"), for: .normal)
+        button.setDimensions(height: 30, width: 30)
+        button.tintColor = MAIN_COLOR
+        button.addTarget(self, action: #selector(attachButtonClicked), for: .touchUpInside)
+        
+        return button
+    }()
+    private lazy var recordVoiceButton: UIButton = {
+        
+        let button = UIButton(type: .system)
+        button.setBackgroundImage(UIImage(systemName: "waveform"), for: .normal)
+        button.setDimensions(height: 30, width: 30)
+        button.tintColor = MAIN_COLOR
+        button.addTarget(self, action: #selector(recordButtonClicked), for: .touchUpInside)
+        
+        return button
+    }()
     private lazy var stackView: UIStackView = {
         
-        let stackView = UIStackView(arrangedSubviews: [inputTextView, postBackgroundColor])
+        let stackView = UIStackView(arrangedSubviews: [inputTextView, postBackgroundColor, attachMediaButton, recordVoiceButton])
         stackView.axis = .horizontal
-        stackView.spacing = 8
+        stackView.spacing = 6
         stackView.alignment = .center
         stackView.distribution = .fillProportionally
         
+        
         return stackView
     }()
+    
+
+    
+    
+    
     
     //MARK: - Lifecycle
     override init(frame: CGRect) {
@@ -67,7 +96,11 @@ class CustomInputView: UIView {
         dividerView.backgroundColor = MAIN_COLOR
         addSubview(dividerView)
         dividerView.anchor(top: topAnchor, left: leftAnchor, right: rightAnchor, height: 0.5)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(textDidChangeProcess), name: InputTextView.textDidChangeNotification, object: nil)
     }
+    
+    
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -87,5 +120,20 @@ class CustomInputView: UIView {
     func clearTextView() {
         inputTextView.text = ""
         inputTextView.placeHolderLabel.isHidden = false
+    }
+    
+    @objc func attachButtonClicked() {
+        print("attch media button clicked")
+    }
+    
+    @objc func recordButtonClicked() {
+        print("record button clicked")
+    }    
+    @objc func textDidChangeProcess() {
+        let isTextEmpty = inputTextView.text.isEmpty || inputTextView.text == ""
+        sendButton.isHidden = isTextEmpty
+        postBackgroundColor.isHidden = isTextEmpty
+        attachMediaButton.isHidden = !isTextEmpty
+        recordVoiceButton.isHidden = !isTextEmpty
     }
 }
